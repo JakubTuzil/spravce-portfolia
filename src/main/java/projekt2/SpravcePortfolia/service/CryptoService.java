c412b52b936b6040b893deca04f6285b77b9ab37
Jakub Tužil <tuzil.ja@gmail.com>
2025-12-26 21:11:42 +0100

    Přidání CryptoService.

--- src/main/java/projekt2/SpravcePortfolia/service/CryptoService.java
+++ src/main/java/projekt2/SpravcePortfolia/service/CryptoService.java
@@ -1,4 +1,40 @@
 package projekt2.SpravcePortfolia.service;
 
+import org.springframework.stereotype.Service;
+import projekt2.SpravcePortfolia.model.Crypto;
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+
+@Service
 public class CryptoService {
+
+    private List<Crypto> cryptoList = new ArrayList<>();
+
+    public void addCrypto(Crypto newCrypto) {
+        cryptoList.add(newCrypto);
+    }
+
+    public List<Crypto> getAllCrypto() {
+        return new ArrayList<>(cryptoList);
+    }
+
+    public List<Crypto> sortByName () {
+        return cryptoList.stream()
+                .sorted(Comparator.comparing(Crypto::getName))
+                .toList();
+    }
+
+    public List<Crypto> sortByPrice () {
+        return cryptoList.stream()
+                .sorted(Comparator.comparing(Crypto::getPrice))
+                .toList();
+    }
+
+    public List<Crypto> sortByQuantity () {
+        return cryptoList.stream()
+                .sorted(Comparator.comparing(Crypto::getQuantity))
+                .toList();
+    }
+
 }
